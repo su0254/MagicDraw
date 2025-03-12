2222ec7ac5f74539f944ee34f48982836ab8285f
su0254 <sarau0254@gmail.com>
2025-03-12 13:03:24 +0200

    starting client side

--- MagicDraw-User/src/App.tsx
+++ MagicDraw-User/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Container, Grid, Button, AppBar, Toolbar, Typography } from '@mui/material';
+import Sidebar from './components/Sidebar';
+import Gallery from './components/Gallery';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container maxWidth="lg">
+      <AppBar position="static" sx={{ backgroundColor: '#3f51b5' }}>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            ציורים לילדים
+          </Typography>
+          <Button color="inherit">אזור אישי</Button>
+          <Button color="inherit">צביעת ציור</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={4} sx={{ marginTop: 2 }}>
+        <Grid item xs={3}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={9}>
+          <Gallery />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default App
+export default App;
