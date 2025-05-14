411fdd4885f585bc852cc9264973700e1e365bdd
su0254 <sarau0254@gmail.com>
2025-05-15 00:06:07 +0300

    add fitcher AI

--- MagicDraw-Angular/MagicDraw-Admin/src/Models/Drawing.ts
+++ MagicDraw-Angular/MagicDraw-Admin/src/Models/Drawing.ts
@@ -2,7 +2,7 @@ export type Drawing = {
 id: string,
 fileName:string,       // מזהה ייחודי
 categoryName: string; // קטגוריה
-// url: string; // כתובת התמונה
+url: string; // כתובת התמונה
 userId: string; // מזהה המשתמש שצייר את התמונה  
 imageFile: File;
 }
--- MagicDraw-Angular/MagicDraw-Admin/src/components/drawings/drawing-dialog/drawing-dialog/drawing-dialog.component.ts
+++ MagicDraw-Angular/MagicDraw-Admin/src/components/drawings/drawing-dialog/drawing-dialog/drawing-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject} from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
@@ -6,32 +6,46 @@ import { MatOption, MatSelect } from '@angular/material/select';
 import { ReactiveFormsModule } from "@angular/forms"
 import { DrawingService } from '../../../../services/drawingService/drawing.service';
 import { MatDialogModule } from '@angular/material/dialog'; // ייבוא MatDialogModule
+import { CategoryService } from '../../../../services/categoryService/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../../../Models/Category';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-drawing-dialog',
-  imports: [ MatLabel, MatError, MatSelect, 
-    MatOption, ReactiveFormsModule,MatDialogModule],
+  imports: [MatLabel, MatError, MatSelect, AsyncPipe,
+    MatOption, ReactiveFormsModule, MatDialogModule],
   templateUrl: './drawing-dialog.component.html',
   styleUrls: ['./drawing-dialog.component.css']
 })
 export class DrawingDialogComponent {
 
-  drawingForm!: FormGroup
+  drawingForm!: FormGroup;
+  newForm: any = new FormGroup({});
+  categories$: Observable<Category[]> | undefined;
   categories: string[] | undefined;
   statuses = ["Approved", "Pending", "Rejected"];
 
   constructor(
     private drawingService: DrawingService,
     private fb: FormBuilder,
+    private categoryService: CategoryService,
     public dialogRef: MatDialogRef<DrawingDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
+  
   ngOnInit() {
-    this.categories= ["Nature", "Animals", "People", "Abstract", "Other"]
+    this.categoryService.getAllCategories();
+    this.categories$ = this.categoryService.categories$;
+    this.categories$.subscribe((categories) => {
+      this.categories = categories.map((category) => category.categoryName);
+    });
+    console.log("categories$", this.categories$);
+    
     this.drawingForm = this.fb.group({
       fileName: [this.data.drawing.title || "", [Validators.required]],
-      categoryName: [this.data.drawing.category || "", [Validators.required]],
+      categoryName: [this.data.drawing.categoryName || "", [Validators.required]],
     })
 
     // Add file upload control for new drawings
@@ -48,7 +62,21 @@ export class DrawingDialogComponent {
     if (this.drawingForm.valid) {
       console.log("drawing-dialog", this.drawingForm.value);
       
-      this.drawingService.addDrawing(this.drawingForm.value).subscribe()
+      this.newForm = this.fb.group({
+        // userId: [this.data.drawing.userId],
+        categoryName: [this.drawingForm.value.categoryName.categoryName],
+        fileName: [this.drawingForm.value.fileName],
+        imageFile: [this.drawingForm.value.imageFile],
+      })
+
+      this.newForm.patchValue({
+        // userId: this.data.drawing.userId,
+        categoryName: this.drawingForm.value.categoryName.categoryName,
+        fileName: this.drawingForm.value.fileName,
+        imageFile: this.drawingForm.value.imageFile,
+      })
+      console.log("newForm", this.newForm.value);
+      this.drawingService.addDrawing(this.newForm.value).subscribe()
       this.dialogRef.close(this.drawingForm.value)
     }
   }
